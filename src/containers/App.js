c82c6e6b3720e43a4a97f9e9ec83d164939a999b
Sara Correia <sara.dsc@hotmail.com>
2020-06-04 14:22:36 +0100

    converted into hoc

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends Component {
   constructor(props) {
@@ -87,7 +88,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes ={classes.App}>
         <button onClick={() => {
           this.setState({ showCockpit: false })
         }}
@@ -99,7 +100,7 @@ class App extends Component {
           personsLength={this.state.persons.length}
           clicked={this.togglePersonsHandler} /> : null}
         {persons}
-      </div>
+      </WithClass> 
     );
   }
 }
